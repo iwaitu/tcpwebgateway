282f2328d7fa9ef51aae27af6a95f499a49610fa
iwaitu <iwaitucn@gmail.com>
2019-08-02 21:35:42 +0800

    传感器消息发布到总线

--- TcpWebGateway/Tools/SensorHelper.cs
+++ TcpWebGateway/Tools/SensorHelper.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using MQTTnet;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using TcpWebGateway.Services;
 

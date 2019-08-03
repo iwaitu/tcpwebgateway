da35b6e512ed4b1493b44796e657901e2dfef1c1
iwaitu <iwaitucn@gmail.com>
2019-08-04 01:35:15 +0800

    ....

---
+++ TcpWebGateway/Tools/SmsHelper.cs
@@ -0,0 +1,44 @@
+﻿using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Exceptions;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Profile;
+using System;
+
+
+namespace TcpWebGateway.Tools
+{
+    public class SmsHelper
+    {
+        public void SendAlert(string sensor)
+        {
+
+        }
+        private void Send(string message,string phoneNo)
+        {
+            IClientProfile profile = DefaultProfile.GetProfile("default", "<accessKeyId>", "<accessSecret>");
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            CommonRequest request = new CommonRequest();
+            request.Method = MethodType.POST;
+            request.Domain = "dysmsapi.aliyuncs.com";
+            request.Version = "2017-05-25";
+            request.Action = "SendSms";
+            // request.Protocol = ProtocolType.HTTP;
+            request.AddQueryParameters("PhoneNumbers", "18107718055");
+            request.AddQueryParameters("SignName", "test");
+            request.AddQueryParameters("TemplateCode", "123123");
+            try
+            {
+                CommonResponse response = client.GetCommonResponse(request);
+                Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
+            }
+            catch (ServerException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ClientException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}

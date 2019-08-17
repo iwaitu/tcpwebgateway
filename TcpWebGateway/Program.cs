89d902e5b4b0819a7ec58db993467803d03953f0
iwaitu <iwaitucn@gmail.com>
2019-08-17 23:03:15 +0800

    ...

--- TcpWebGateway/Program.cs
+++ TcpWebGateway/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
@@ -38,7 +40,13 @@ namespace TcpWebGateway
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder().ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 8080, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                });
+            })
                 .UseStartup<Startup>()
             .ConfigureLogging(logging =>
             {

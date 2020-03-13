938ae09f1ef491bd609b488b20da6287e3c5c405
iwaitu <iwaitucn@gmail.com>
2020-03-14 03:07:14 +0800

    ...

---
+++ TcpWebGateway/Controllers/LogsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TcpWebGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        public LogsController()
+        {
+            
+        }
+    }
+}
---
+++ TcpWebGateway/models/LogItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TcpWebGateway.models
+{
+    public class LogItem
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+    }
+}
